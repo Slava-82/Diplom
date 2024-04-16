6af2ec1d66da81adf42dcdce315ed0b0160de3fa
Marina Kononova <marinakononova@mail.ru>
2024-04-16 14:28:26 +0400

    One Test added

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -3,19 +3,57 @@ package ru.netology.javaqadiplom;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-//public class SavingAccountTest {
-//
-//    @Test
-//    public void shouldAddLessThanMaxBalance() {
-//        SavingAccount account = new SavingAccount(
-//                2_000,
-//                1_000,
-//                10_000,
-//                5
-//        );
-//
-//        account.add(3_000);
-//
-//        Assertions.assertEquals(2_000 + 3_000, account.getBalance());
-//    }
-//}
+public class SavingAccountTest {
+/*
+    @Test
+    void ShouldCreateSavingAccountRateMinus1() {
+        SavingAccount savingAcc = new SavingAccount(
+                0,
+                0,
+                50_000,
+                -1
+        );
+        //Assertions.assertEquals(0, savingAcc.getRate());
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            savingAcc.getRate();
+        });
+    }
+
+ */
+
+    @Test
+    void ShouldCreateSavingAccount() {
+        SavingAccount savingAcc = new SavingAccount(
+                0,
+                0,
+                50_000,
+                5
+        );
+        Assertions.assertEquals(0, savingAcc.getBalance());
+        Assertions.assertEquals(0, savingAcc.getMinBalance());
+        Assertions.assertEquals(50_000, savingAcc.getMaxBalance());
+        Assertions.assertEquals(5, savingAcc.getRate());
+    }
+
+    @Test
+    void pay() {
+        SavingAccount savingAcc = new SavingAccount(
+                0,
+                0,
+                50_000,
+                10
+        );
+
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void yearChange() {
+    }
+
+}
+
