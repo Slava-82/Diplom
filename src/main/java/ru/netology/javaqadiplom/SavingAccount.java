e9aeb33e3880b3a67975488d2f4c94bda88ff8b5
Slava-82 <utsznslava@gmail.com>
2024-04-19 23:18:57 +0300

    Class fix

--- src/main/java/ru/netology/javaqadiplom/SavingAccount.java
+++ src/main/java/ru/netology/javaqadiplom/SavingAccount.java
@@ -1,166 +1,3 @@
-//package ru.netology.javaqadiplom;
-//
-///**
-// * Сберегательный счёт
-// * Может иметь баланс только в пределах от указанного минимального до указанного максимального включительно.
-// * Не может уходить в минус (минимальный баланс не может быть отрицательным).
-// * Имеет ставку - количество процентов годовых на остаток.
-// */
-//public class SavingAccount extends Account {
-//    protected int minBalance;
-//    protected int maxBalance;
-//
-//    /**
-//     * Создаёт новый объект сберегательного счёта с заданными параметрами.
-//     * Если параметры некорректны (мин. баланс больше максимального и так далее), то
-//     * должно выкидываться исключения вида IllegalArgumentException.
-//     *
-//     * @param initialBalance - начальный баланс
-//     * @param minBalance     - минимальный баланс
-//     * @param maxBalance     - максимальный баланс
-//     * @param rate           - неотрицательное число, ставка в процентах годовых на остаток
-//     */
-//    public SavingAccount(int initialBalance, int minBalance, int maxBalance, int rate) {
-//        if (rate < 0) {
-//            throw new IllegalArgumentException(
-//                    "Накопительная ставка не может быть отрицательной, а у вас: " + rate
-//            );
-//        }
-//        this.balance = initialBalance;
-//        this.minBalance = minBalance;
-//        this.maxBalance = maxBalance;
-//        this.rate = rate;
-//
-//        if (initialBalance < 0) {
-//            throw new IllegalArgumentException(
-//                    "Начальный баланс не может быть отрицательный, а у вас: " + initialBalance
-//            );
-//        }
-//        this.balance = initialBalance;
-//        this.minBalance = minBalance;
-//        this.maxBalance = maxBalance;
-//        this.rate = rate;
-//
-//        if (minBalance < 0) {
-//            throw new IllegalArgumentException(
-//                    "Минимальный баланс не может быть отрицательный, а у вас: " + minBalance
-//            );
-//        }
-//        this.balance = initialBalance;
-//        this.minBalance = minBalance;
-//        this.maxBalance = maxBalance;
-//        this.rate = rate;
-//
-//        if (maxBalance < 0) {
-//            throw new IllegalArgumentException(
-//                    "Максимальный баланс не может быть отрицательный, а у вас: " + maxBalance
-//            );
-//        }
-//        this.balance = initialBalance;
-//        this.minBalance = minBalance;
-//        this.maxBalance = maxBalance;
-//        this.rate = rate;
-//
-//        if (minBalance > maxBalance) {
-//            throw new IllegalArgumentException(
-//                    "Минимальный баланс не может быть больше максимального баланса, а у вас: "
-//                            + " минимальный баланс: " + minBalance + " больше максимального баланса: " + maxBalance
-//            );
-//        }
-//        this.balance = initialBalance;
-//        this.minBalance = minBalance;
-//        this.maxBalance = maxBalance;
-//        this.rate = rate;
-//
-//        if (initialBalance < minBalance) {
-//            throw new IllegalArgumentException(
-//                    "Баланс не может быть меньше минимального баланса, а у вас: "
-//                            + " баланс: " + initialBalance + " меньше минимального баланса: " + minBalance
-//            );
-//        }
-//        this.balance = initialBalance;
-//        this.minBalance = minBalance;
-//        this.maxBalance = maxBalance;
-//        this.rate = rate;
-//
-//        if (initialBalance > maxBalance) {
-//            throw new IllegalArgumentException(
-//                    "Баланс не может быть больше максимального баланса, а у вас: "
-//                            + " баланс: " + initialBalance + " больше максимального баланса: " + maxBalance
-//            );
-//        }
-//        this.balance = initialBalance;
-//        this.minBalance = minBalance;
-//        this.maxBalance = maxBalance;
-//        this.rate = rate;
-//    }
-//
-//    /**
-//     * Операция оплаты с карты на указанную сумму.
-//     * В результате успешного вызова этого метода, баланс должен уменьшиться
-//     * на сумму покупки. Если же операция может привести к некорректному
-//     * состоянию счёта (например, баланс может уйти в минус), то операция должна
-//     * завершиться вернув false и ничего не поменяв на счёте.
-//     *
-//     * @param amount - сумма покупки
-//     * @return true если операция прошла успешно, false иначе.
-//     */
-//    @Override
-//    public boolean pay(int amount) {
-//        if (amount <= 0 || balance - amount < minBalance) {
-//            return false;
-//        }
-//        balance -= amount;
-//        return true;
-//    }
-//
-//    /**
-//     * Операция пополнения карты на указанную сумму.
-//     * В результате успешного вызова этого метода, баланс должен увеличиться
-//     * на сумму покупки. Если же операция может привести к некорректному
-//     * состоянию счёта, то операция должна
-//     * завершиться вернув false и ничего не поменяв на счёте.
-//     *
-//     * @param amount - сумма пополнения
-//     * @param amount
-//     * @return true если операция прошла успешно, false иначе.
-//     * @return
-//     */
-//
-//    @Override
-//    public boolean add(int amount) {
-//        if (amount <= 0 || balance + amount > maxBalance) {
-//            return false;
-//        } else {
-//            balance += amount;
-//            return true;
-//        }
-//    }
-//
-//    /**
-//     * Операция расчёта процентов на остаток счёта при условии, что
-//     * счёт не будет меняться год. Сумма процентов приводится к целому
-//     * числу через отбрасывание дробной части (так и работает целочисленное деление).
-//     * Пример: если на счёте 200 рублей, то при ставке 15% ответ должен быть 30.
-//     *
-//     * @return
-//     */
-//    @Override
-//    public int yearChange() {
-//        return balance / 100 * rate;
-//    }
-//
-//    public int getMinBalance() {
-//        return minBalance;
-//    }
-//
-//    public int getMaxBalance() {
-//        return maxBalance;
-//    }
-//}
-
-
-
 package ru.netology.javaqadiplom;
 
 /**
@@ -190,40 +27,24 @@ public class SavingAccount extends Account {
 
             );
         }
-        this.balance = initialBalance;
-        this.minBalance = minBalance;
-        this.maxBalance = maxBalance;
-        this.rate = rate;
 
         if (initialBalance < 0) {
             throw new IllegalArgumentException(
                     "Начальный баланс не может быть отрицательный, а у вас: " + initialBalance
             );
         }
-        this.balance = initialBalance;
-        this.minBalance = minBalance;
-        this.maxBalance = maxBalance;
-        this.rate = rate;
 
         if (minBalance < 0) {
             throw new IllegalArgumentException(
                     "Минимальный баланс не может быть отрицательный, а у вас: " + minBalance
             );
         }
-        this.balance = initialBalance;
-        this.minBalance = minBalance;
-        this.maxBalance = maxBalance;
-        this.rate = rate;
 
         if (maxBalance < 0) {
             throw new IllegalArgumentException(
                     "Максимальный баланс не может быть отрицательный, а у вас: " + maxBalance
             );
         }
-        this.balance = initialBalance;
-        this.minBalance = minBalance;
-        this.maxBalance = maxBalance;
-        this.rate = rate;
 
         if (minBalance > maxBalance) {
             throw new IllegalArgumentException(
@@ -231,10 +52,6 @@ public class SavingAccount extends Account {
                             + " минимальный баланс: " + minBalance + " больше максимального баланса: " + maxBalance
             );
         }
-        this.balance = initialBalance;
-        this.minBalance = minBalance;
-        this.maxBalance = maxBalance;
-        this.rate = rate;
 
         if (initialBalance < minBalance) {
             throw new IllegalArgumentException(
@@ -242,10 +59,6 @@ public class SavingAccount extends Account {
                             + " начальный баланс: " + initialBalance + " меньше минимального баланса: " + minBalance
             );
         }
-        this.balance = initialBalance;
-        this.minBalance = minBalance;
-        this.maxBalance = maxBalance;
-        this.rate = rate;
 
         if (initialBalance > maxBalance) {
             throw new IllegalArgumentException(
@@ -321,4 +134,3 @@ public class SavingAccount extends Account {
         return maxBalance;
     }
 }
-//zeu
