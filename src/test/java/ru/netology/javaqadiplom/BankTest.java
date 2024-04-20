49fb138a207d3171591d5e9ad48338a109b0cfd0
Marina Kononova <marinakononova@mail.ru>
2024-04-21 00:33:26 +0400

    Reformated

--- src/test/java/ru/netology/javaqadiplom/BankTest.java
+++ src/test/java/ru/netology/javaqadiplom/BankTest.java
@@ -3,8 +3,6 @@ package ru.netology.javaqadiplom;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class BankTest {
     //credit - credit
     @Test
